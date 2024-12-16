5f5176332c83143a947c2e5a14afdb3930c32d09
unknown <KK201431873@gmail.com>
2024-12-16 12:21:21 -0800

    new dropper code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/calibration/SpecimenMakerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/calibration/SpecimenMakerTest.java
@@ -11,17 +11,73 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 public class SpecimenMakerTest extends LinearOpMode {
 
     Servo specimenClipper;
-    public static double servoPosition;
+    Servo specimenDropper;
+
+    public static double specimenClipperDeactivatedPosition = 0;
+    public static double specimenClipperActivatedPosition = 0.3;
+
+    public static double specimenDropperDeactivatedPosition = 0.5;
+    public static double specimenDropperActivatedPosition = 0.7;
+
+    public int specimenMakerState = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
         specimenClipper = hardwareMap.get(ServoImplEx.class, "specimenClipper");
-        servoPosition = 0;
+        double specimenClipperPosition = 0;
+
+        specimenDropper = hardwareMap.get(ServoImplEx.class, "specimenDropper");
+        double specimenDropperPosition = 0;
 
         waitForStart();
 
+        boolean rbPressed = false;
+        boolean lbPressed = false;
+        int n_states = 4;
         while (opModeIsActive()) {
-            specimenClipper.setPosition(servoPosition);
+
+            // LB = Go back state
+            // RB = Advance state
+
+            if (gamepad1.left_bumper && !lbPressed) {
+                lbPressed = true;
+            } else if (!gamepad1.left_bumper & lbPressed) {
+                lbPressed = false;
+                specimenMakerState = specimenMakerState>0 ? ((specimenMakerState - 1)%n_states) : 0;
+            }
+
+            if (gamepad1.right_bumper && !rbPressed) {
+                rbPressed = true;
+            } else if (!gamepad1.right_bumper & rbPressed) {
+                rbPressed = false;
+                specimenMakerState = (specimenMakerState + 1)%n_states;
+            }
+
+
+
+            // Control servos
+
+            if (specimenMakerState == 0) {
+                specimenDropperPosition = specimenDropperDeactivatedPosition;
+                specimenClipperPosition = specimenClipperDeactivatedPosition;
+            }
+            if (specimenMakerState == 1) {
+                specimenDropperPosition = specimenDropperActivatedPosition;
+                specimenClipperPosition = specimenClipperDeactivatedPosition;
+            }
+            if (specimenMakerState == 2) {
+                specimenDropperPosition = specimenDropperActivatedPosition;
+                specimenClipperPosition = specimenClipperActivatedPosition;
+            }
+            if (specimenMakerState == 3) {
+                specimenDropperPosition = specimenDropperActivatedPosition;
+                specimenClipperPosition = specimenClipperDeactivatedPosition;
+            }
+
+            specimenClipper.setPosition(specimenClipperPosition);
+            specimenDropper.setPosition(specimenDropperPosition);
+
+
         }
 
     }
