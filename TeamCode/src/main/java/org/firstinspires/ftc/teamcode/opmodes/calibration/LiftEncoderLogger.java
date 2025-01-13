a06a9babaae1222b1e63df0398befe956da2fa70
unknown <KK201431873@gmail.com>
2025-01-13 14:17:56 -0800

    lift encoder logger pid controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/calibration/LiftEncoderLogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/calibration/LiftEncoderLogger.java
@@ -1,19 +1,38 @@
 package org.firstinspires.ftc.teamcode.opmodes.calibration;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.ArrayList;
 
 
+@Config
 @TeleOp(name = "Lift Encoder Logger", group = "Calibration")
 public class LiftEncoderLogger extends LinearOpMode {
 
     private MotorEx leftLift;
     private MotorEx rightLift;
+
+    public static double kP = 0.01;
+    public static double kI = 0.01;
+    public static double kD = 0;
+    private ArrayList<Double> lastErrors = new ArrayList<>();
+    private ArrayList<Double> deltaTimes = new ArrayList<>();
+    private double errorSum = 0;
+    private ElapsedTime runtime;
+
+    public static double speed = 0.5;
+
+    public static double joyspeed = 10;
+    private double encoderSetpoint = 0;
+    public static double maxTicks = 1000;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -26,13 +45,14 @@ public class LiftEncoderLogger extends LinearOpMode {
         leftLift.setRunMode(Motor.RunMode.RawPower);
         leftLift.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftLift.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-        leftLift.setInverted(true);
+        leftLift.setInverted(false);
 
         rightLift.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightLift.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightLift.setRunMode(Motor.RunMode.RawPower);
         rightLift.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightLift.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightLift.setInverted(true);
 
         telemetry.addData("[TICKS] Left", 0);
         telemetry.addData("[TICKS] Right", 0);
@@ -41,9 +61,10 @@ public class LiftEncoderLogger extends LinearOpMode {
         telemetry.addData("[VELOCITY] Left", 0);
         telemetry.update();
 
+        runtime = new ElapsedTime(0);
         waitForStart();
 
-        double lastPosition = 0;
+        runtime.reset();
         while (opModeIsActive()) {
             // CONTROLS
             //
@@ -77,15 +98,62 @@ public class LiftEncoderLogger extends LinearOpMode {
             leftPower = clamp(leftPower);
             rightPower = clamp(rightPower);
 
+
+
+
+
             // Set powers
-            leftLift.set(leftPower);
-            rightLift.set(rightPower);
+            leftLift.set(speed*leftPower);
+            if (gamepad1.right_bumper) {
+                double deltaTime = runtime.seconds();
+                // setpoint
+                encoderSetpoint += -gamepad1.left_stick_y*joyspeed*deltaTime;
+                encoderSetpoint = Math.min(Math.max(0, encoderSetpoint),maxTicks);
+                // PID controller
+                double pv = rightLift.getCurrentPosition();
+                double e = encoderSetpoint - pv;
+                runtime.reset();
+
+                // de/dt
+                lastErrors.add(e);
+                deltaTimes.add(deltaTime);
+                double derivative = 0;
+                if (lastErrors.size() > 5) {
+                    lastErrors.remove(0);
+                    deltaTimes.remove(0);
+                    double avgDeltaTime = deltaTimes.stream()
+                            .mapToDouble(d -> d)
+                            .average()
+                            .orElse(0.0);
+                    derivative = (-lastErrors.get(0)+8*lastErrors.get(1)-8*lastErrors.get(3)+lastErrors.get(4))/
+                            (12*avgDeltaTime);
+                }
+
+                // int edt
+                errorSum += deltaTime*e;
+                if (lastErrors.size()>1) {
+                    if (lastErrors.get(lastErrors.size()-2)*e <= 0) {
+                        // flush integral stack
+                        errorSum = 0;
+                    }
+                }
+
+                double ut = kP*e + kI*errorSum + kD*derivative;
+                rightLift.set(ut);
+                telemetry.addData("error", e);
+                telemetry.addData("errorSum", errorSum);
+                telemetry.addData("errorDerivative", derivative);
+                telemetry.addData("ut", ut);
+            } else {
+                rightLift.set(speed*rightPower);
+            }
 
             // Telemetry
             telemetry.addData("[TICKS] Left", leftLift.getCurrentPosition());
             telemetry.addData("[TICKS] Right", rightLift.getCurrentPosition());
             telemetry.addData("[POWER] Left", leftPower);
             telemetry.addData("[POWER] Right", rightPower);
+            telemetry.addData("[TICKS] setpoint", encoderSetpoint);
             telemetry.update();
         }
 
