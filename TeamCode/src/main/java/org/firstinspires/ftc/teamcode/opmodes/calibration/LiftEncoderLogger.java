0adc7ccdaa0e1624c1140d917c66d509585364a6
unknown <KK201431873@gmail.com>
2025-01-17 12:55:59 -0800

    cascading slides (just retuned)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/calibration/LiftEncoderLogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/calibration/LiftEncoderLogger.java
@@ -21,18 +21,21 @@ public class LiftEncoderLogger extends LinearOpMode {
     private MotorEx rightLift;
 
     public static double kP = 0.01;
-    public static double kI = 0.01;
+    public static double kI = 0.002;
     public static double kD = 0;
-    private ArrayList<Double> lastErrors = new ArrayList<>();
+    private ArrayList<Double> lastLeftErrors = new ArrayList<>();
+    private ArrayList<Double> lastRightErrors = new ArrayList<>();
     private ArrayList<Double> deltaTimes = new ArrayList<>();
-    private double errorSum = 0;
+    private double leftErrorSum = 0;
+    private double rightErrorSum = 0;
     private ElapsedTime runtime;
 
     public static double speed = 0.5;
 
     public static double joyspeed = 10;
-    private double encoderSetpoint = 0;
-    public static double maxTicks = 1000;
+    private double encoderLeftSetpoint = 0;
+    private double encoderRightSetpoint = 0;
+    public static double maxTicks = 1100;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -103,48 +106,89 @@ public class LiftEncoderLogger extends LinearOpMode {
 
 
             // Set powers
-            leftLift.set(speed*leftPower);
-            if (gamepad1.right_bumper) {
+            if (gamepad1.right_bumper || gamepad1.left_bumper) {
                 double deltaTime = runtime.seconds();
-                // setpoint
-                encoderSetpoint += -gamepad1.left_stick_y*joyspeed*deltaTime;
-                encoderSetpoint = Math.min(Math.max(0, encoderSetpoint),maxTicks);
-                // PID controller
-                double pv = rightLift.getCurrentPosition();
-                double e = encoderSetpoint - pv;
                 runtime.reset();
 
-                // de/dt
-                lastErrors.add(e);
-                deltaTimes.add(deltaTime);
-                double derivative = 0;
-                if (lastErrors.size() > 5) {
-                    lastErrors.remove(0);
-                    deltaTimes.remove(0);
-                    double avgDeltaTime = deltaTimes.stream()
-                            .mapToDouble(d -> d)
-                            .average()
-                            .orElse(0.0);
-                    derivative = (-lastErrors.get(0)+8*lastErrors.get(1)-8*lastErrors.get(3)+lastErrors.get(4))/
-                            (12*avgDeltaTime);
-                }
+                // setpoint
+                if (gamepad1.left_bumper) {
+                    encoderLeftSetpoint += -gamepad1.left_stick_y * joyspeed * deltaTime;
+                    encoderLeftSetpoint = Math.min(Math.max(0, encoderLeftSetpoint), maxTicks);
+                    // PID controller
+                    double pv = leftLift.getCurrentPosition();
+                    double e = encoderLeftSetpoint - pv;
 
-                // int edt
-                errorSum += deltaTime*e;
-                if (lastErrors.size()>1) {
-                    if (lastErrors.get(lastErrors.size()-2)*e <= 0) {
-                        // flush integral stack
-                        errorSum = 0;
+                    // de/dt
+                    lastLeftErrors.add(e);
+                    deltaTimes.add(deltaTime);
+                    double derivative = 0;
+                    if (lastLeftErrors.size() > 5) {
+                        lastLeftErrors.remove(0);
+                        deltaTimes.remove(0);
+                        double avgDeltaTime = deltaTimes.stream()
+                                .mapToDouble(d -> d)
+                                .average()
+                                .orElse(0.0);
+                        derivative = (-lastLeftErrors.get(0) + 8 * lastLeftErrors.get(1) - 8 * lastLeftErrors.get(3) + lastLeftErrors.get(4)) /
+                                (12 * avgDeltaTime);
                     }
-                }
 
-                double ut = kP*e + kI*errorSum + kD*derivative;
-                rightLift.set(ut);
-                telemetry.addData("error", e);
-                telemetry.addData("errorSum", errorSum);
-                telemetry.addData("errorDerivative", derivative);
-                telemetry.addData("ut", ut);
+                    // int edt
+                    leftErrorSum += deltaTime * e;
+                    if (lastLeftErrors.size() > 1) {
+                        if (lastLeftErrors.get(lastLeftErrors.size() - 2) * e <= 0) {
+                            // flush integral stack
+                            leftErrorSum = 0;
+                        }
+                    }
+
+                    double ut = kP * e + kI * leftErrorSum + kD * derivative;
+                    leftLift.set(ut);
+                    telemetry.addData("leftError", e);
+                    telemetry.addData("leftErrorSum", leftErrorSum);
+                    telemetry.addData("leftErrorDerivative", derivative);
+                    telemetry.addData("left_ut", ut);
+                }
+                if (gamepad1.right_bumper) {
+                    encoderRightSetpoint += -gamepad1.left_stick_y * joyspeed * deltaTime;
+                    encoderRightSetpoint = Math.min(Math.max(0, encoderRightSetpoint), maxTicks);
+                    // PID controller
+                    double pv = rightLift.getCurrentPosition();
+                    double e = encoderRightSetpoint - pv;
+
+                    // de/dt
+                    lastRightErrors.add(e);
+                    deltaTimes.add(deltaTime);
+                    double derivative = 0;
+                    if (lastRightErrors.size() > 5) {
+                        lastRightErrors.remove(0);
+                        deltaTimes.remove(0);
+                        double avgDeltaTime = deltaTimes.stream()
+                                .mapToDouble(d -> d)
+                                .average()
+                                .orElse(0.0);
+                        derivative = (-lastRightErrors.get(0) + 8 * lastRightErrors.get(1) - 8 * lastRightErrors.get(3) + lastRightErrors.get(4)) /
+                                (12 * avgDeltaTime);
+                    }
+
+                    // int edt
+                    rightErrorSum += deltaTime * e;
+                    if (lastRightErrors.size() > 1) {
+                        if (lastRightErrors.get(lastRightErrors.size() - 2) * e <= 0) {
+                            // flush integral stack
+                            rightErrorSum = 0;
+                        }
+                    }
+
+                    double ut = kP * e + kI * rightErrorSum + kD * derivative;
+                    rightLift.set(ut);
+                    telemetry.addData("rightError", e);
+                    telemetry.addData("rightErrorSum", rightErrorSum);
+                    telemetry.addData("rightErrorDerivative", derivative);
+                    telemetry.addData("right_ut", ut);
+                }
             } else {
+                leftLift.set(speed*leftPower);
                 rightLift.set(speed*rightPower);
             }
 
@@ -153,7 +197,8 @@ public class LiftEncoderLogger extends LinearOpMode {
             telemetry.addData("[TICKS] Right", rightLift.getCurrentPosition());
             telemetry.addData("[POWER] Left", leftPower);
             telemetry.addData("[POWER] Right", rightPower);
-            telemetry.addData("[TICKS] setpoint", encoderSetpoint);
+            telemetry.addData("[TICKS] leftSetpoint", encoderLeftSetpoint);
+            telemetry.addData("[TICKS] rightSetpoint", encoderRightSetpoint);
             telemetry.update();
         }
 
