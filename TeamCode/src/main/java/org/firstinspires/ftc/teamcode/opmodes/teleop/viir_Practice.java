4d61967104aecbaf1e96c1e77d5170cc2d5f6773
Your Name <you@example.com>
2025-02-26 10:50:39 -0500

    iweejfaewf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/viir_Practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/viir_Practice.java
@@ -1,4 +1,55 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
-public class viir_Practice {
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.HardwareRobot;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+
+
+@TeleOp (name = "ViirTeleop")
+
+//don't merge changes, private branch.
+public class viir_Practice extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        HardwareRobot robot = new HardwareRobot(hardwareMap);
+        DriveSubsystem drive = new DriveSubsystem(
+                robot.leftFront,
+                robot.rightFront,
+                robot.leftBack,
+                robot.rightBack
+        );
+        waitForStart();
+        while (opModeIsActive()) {
+            double speed = 0.9;
+            boolean breakLoopHehe = false;
+            if (gamepad1.square && !breakLoopHehe) {
+                breakLoopHehe = true;
+            }
+            if (breakLoopHehe) {
+                telemetry.addData("You have been hacked", 9);
+                telemetry.update();
+            }
+            if (!gamepad1.square) {
+                breakLoopHehe = false;
+            }
+            double strafe = -gamepad1.left_stick_x * speed;
+            double forward = gamepad1.left_stick_y * speed;
+            double turn = gamepad1.right_stick_x * speed;
+            drive.driveRobotCentric(strafe, forward, turn);
+            double buffer = 0.2;
+            boolean senseChangeActivated = false;
+            if (gamepad1.circle && !senseChangeActivated) {
+                senseChangeActivated = true;
+            }
+            if (senseChangeActivated) {
+            }
+            if (senseChangeActivated && gamepad1.circle) {
+                senseChangeActivated = false;
+            }
+
+        }
+    }
 }
