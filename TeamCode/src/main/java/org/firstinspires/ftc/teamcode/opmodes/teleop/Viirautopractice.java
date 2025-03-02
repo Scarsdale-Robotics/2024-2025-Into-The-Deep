b5b94d602901caa5210934774d47b50012a3002a
Your Name <you@example.com>
2025-03-02 00:26:04 -0500

    one more thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Viirautopractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Viirautopractice.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 //plans and states: specimen maker
 //auto and cv, teleop: spec-mkr and sync macro
 //branches in general - main, specimen maker, spec-mkr, synchronizer-macro
+//ALSO LOOK AT AND UNDERSTAND OLD TELEOP AND ERROR STUFF FOR MACROS RUNNING
 @Autonomous (name = "Sigma autunomous for old robot")
 public class Viirautopractice extends LinearOpMode {
     @Override
