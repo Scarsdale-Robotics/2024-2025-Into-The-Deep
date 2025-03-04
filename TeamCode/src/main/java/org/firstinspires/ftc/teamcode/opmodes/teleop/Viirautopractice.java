0ef0517a2bceffaab3c0d64018a1fe1d78c4c21f
Your Name <you@example.com>
2025-03-04 10:22:26 -0500

    spanish class lmao

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Viirautopractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Viirautopractice.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.HardwareRobot;
@@ -11,13 +13,12 @@ import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 //auto and cv, teleop: spec-mkr and sync macro
 //branches in general - main, specimen maker, spec-mkr, synchronizer-macro
 //ALSO LOOK AT AND UNDERSTAND OLD TELEOP AND ERROR STUFF FOR MACROS RUNNING
-@Autonomous (name = "Sigma autunomous for old robot")
+@Autonomous (name = "Sigma autonomous for old robot")
 public class Viirautopractice extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
       while (opModeIsActive()) {
-          double asidhfsadf = 0.3;
           HardwareRobot robot = new HardwareRobot(hardwareMap);
           DriveSubsystem drive = new DriveSubsystem (
             robot.leftBack,
@@ -25,7 +26,9 @@ public class Viirautopractice extends LinearOpMode {
             robot.rightBack,
             robot.rightFront
           );
+          MultipleTelemetry telemetry = new MultipleTelemetry(this.telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry()));
           waitForStart();
+
 
       }
     }
