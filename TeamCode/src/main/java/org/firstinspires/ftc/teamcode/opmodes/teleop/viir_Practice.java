db3b15012d0bc573a4c8049578c23b144277de34
Your Name <you@example.com>
2025-03-09 19:11:43 -0400

    bus changes to old robot practice code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/viir_Practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/viir_Practice.java
@@ -12,10 +12,11 @@ import org.firstinspires.ftc.teamcode.synchropather.systems.claw.ClawConstants;
 import org.firstinspires.ftc.teamcode.synchropather.systems.elbow.ElbowConstants;
 
 @TeleOp(name = "ViirTeleop")
-//hello just random stuff i do here to practice, i know a lot more than i can write trust
-//TODO: fix telemetry, elbow and claw change booleans
-//TODO: add realign macro and reset heading macro, add lift stuff.
-//TODO: implement control theory in some code (error and stuff)
+//hello just random stuff i do here to practice, i know a lot more than i can write trust.
+//upate 3/9 on way back from bus: we clutched up. i learned odometry, localization, and cv in one bus reide les goooo
+//TODO: ADD, COMMIT, PUSH CHANGES WHEN GET HOME AND CHECKOUT TO SYNCHRONIZER-MACRO BRANCH AFTER DONE
+//TODO: Viir's code: add lift stuff.
+//TODO: Viir's code: implement control theory in some code (error and stuff) for running more complicated macros
 //IMPORTANT: don't merge changes to sync macro or main, private branch.
 public class viir_Practice extends LinearOpMode {
     public static double clawClosed = ClawConstants.CLOSED_POSITION;
@@ -31,7 +32,7 @@ public class viir_Practice extends LinearOpMode {
         //init
         this.robot = new RobotSystem(hardwareMap, new Pose2d(0, 0, new Rotation2d(Math.toRadians(-90))), false, this);
 
-        //telemetry stufff (find out meaning of multipletelemetry)
+        //telemetry stuff (find out meaning of multiple telemetry)
         robot.inDep.setClawPosition(clawPos);
         robot.inDep.setElbowPosition(elbowPos);
         waitForStart();
@@ -44,24 +45,20 @@ public class viir_Practice extends LinearOpMode {
             boolean displayTelemetry = false;
             boolean isClawOpen = false;
             boolean isElbowDown = false;
+            double random = 0.234;
             boolean senseChangeActivated = false;
             double strafe = -gamepad1.left_stick_x * speed;
             double forward = gamepad1.left_stick_y * speed;
             double turn = gamepad1.right_stick_x * speed;
             double buffer = 0.2;
-            if (gamepad1.square && !displayTelemetry) {
-                displayTelemetry = true;
-            }
-            if (displayTelemetry) {
-                telemetry.addData("Strafe: ", strafe);
-                telemetry.addData("Turn: ", turn);
-                telemetry.addData("Forward: ", forward);
-                telemetry.addData("Speed: ", speed);
-                telemetry.addData("Buffer for sense change: ", buffer);
-                telemetry.addData("Viir is a ", viir);
-                telemetry.update();
-            }
-            if (gamepad1.x && !isElbowDown) {
+            telemetry.addData("Strafe: ", strafe);
+            telemetry.addData("Turn: ", turn);
+            telemetry.addData("Forward: ", forward);
+            telemetry.addData("Speed: ", speed);
+            telemetry.addData("Buffer for sense change: ", buffer);
+            telemetry.addData("Viir is a ", viir);
+            telemetry.update();
+            if (gamepad1.x) {
                 isElbowDown = true;
             }
             if (isElbowDown) {
@@ -73,7 +70,7 @@ public class viir_Practice extends LinearOpMode {
                 elbowPos = elbowUp;
                 robot.inDep.setElbowPosition(elbowPos);
             }
-            if (gamepad1.circle && !isClawOpen) {
+            if (gamepad1.circle) {
                 isClawOpen = true;
             }
             if (isClawOpen) {
@@ -85,13 +82,10 @@ public class viir_Practice extends LinearOpMode {
                 clawPos = clawClosed;
                 robot.inDep.setClawPosition(clawPos);
             }
-            if (gamepad1.square && displayTelemetry) {
-                displayTelemetry = false;
-            }
             //field centric drive: includes angles for more accuracy with regards to human perspective
             //get H - get heading
             robot.drive.driveFieldCentric(strafe , forward, turn, Math.toDegrees(robot.localization.getH()));
-            if (gamepad1.circle && !senseChangeActivated) {
+            if (gamepad1.circle) {
                 senseChangeActivated = true;
             }
             if (senseChangeActivated) {
@@ -106,7 +100,7 @@ public class viir_Practice extends LinearOpMode {
                 forward = gamepad1.left_stick_y * speed;
             }
             boolean speedUpToggled = false;
-            if (gamepad1.a && !speedUpToggled) {
+            if (gamepad1.a) {
                 speedUpToggled = true;
             }
             if (speedUpToggled) {
@@ -116,7 +110,27 @@ public class viir_Practice extends LinearOpMode {
                 speedUpToggled = false;
                 speed = 0.9;
             }
-
+            if (gamepad1.dpad_down) {
+                realignMacro();
+            }
+            if (gamepad1.dpad_up) {
+                resetServos();
+                telemetry.addData("Reset of Claw and Elbow Successful.", random);
+                telemetry.update();
+            }
         }
+    }
+    public void realignMacro() {
+        robot.localization.resetH(Math.toRadians(-180));
+        double currentHeading = robot.localization.getH();
+        robot.localization.update();
+        robot.logOdometry();
+        telemetry.addData("Reset Sucessful. Heading: ", currentHeading);
+    }
+    public void resetServos() {
+        clawPos = clawClosed;
+        elbowPos = elbowUp;
+        robot.inDep.setClawPosition(clawPos);
+        robot.inDep.setElbowPosition(elbowPos);
     }
 }
