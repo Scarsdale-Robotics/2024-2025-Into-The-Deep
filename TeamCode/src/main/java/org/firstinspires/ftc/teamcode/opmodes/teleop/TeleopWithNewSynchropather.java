a830ee3424a3dd670ddd3e21816857990cdf0e1d
Your Name <you@example.com>
2025-03-13 18:14:02 -0400

    minor changes to synchro teleop, created new classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleopWithNewSynchropather.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleopWithNewSynchropather.java
@@ -9,11 +9,27 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.RobotSystem;
 @TeleOp(name = "sigma")
+//IMPORTANT - I was being lazy and didnt want to merge with the 4 other branches that have Krakens code
+//so, this is for the old robot. sry if that creates any problems
 public class TeleopWithNewSynchropather extends LinearOpMode {
     private RobotSystem robot;
     public void runOpMode() {
-    while (opModeIsActive()) {
+        /**regular initalization...i also don't know what the difference
+         *    between robotsystem and autonomousrobot is so ill use robotsys for now
+         */
         this.robot = new RobotSystem(hardwareMap, new Pose2d(0, 0, new Rotation2d(Math.toRadians(-90))), false, this);
+        robot.logOdometry();
+        robot.localization.update();
+        waitForStart();
+        double speed = 1;
+    while (opModeIsActive()) {
+        //regular field centric drive, doesnt really matter
+        //i also keep forgetting strafe is inverted smh
+        double strafe = -gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+        double forward = gamepad1.left_stick_y;
+        robot.drive.driveFieldCentricPowers(strafe, forward, turn, Math.toDegrees(robot.localization.getH()));
+        //also confused about fieldcentric vs fieldcentricpowers....
     }
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/synchropather/systems/__util__/Command.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.synchropather.systems.__util__;
+
+public class Command {
+    public double speed;
+    public String initMotor1;
+    public String initMotor2;
+    public String initMotor3;
+    public String initMotor4;
+    public void resetTags() {
+        //ill add this later, gotta study now :(
+        //I'm starting to think this may be a really good idea.....
+    }
+}
